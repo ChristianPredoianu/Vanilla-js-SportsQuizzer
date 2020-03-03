775e0070a8b48bb90ca8607e534f5da41f036a4e
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2020-03-03 12:44:42 +0100

    impl reset quiz & show correct answer

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const questionsAmount = document.getElementById('amount-of-questions');
 const startQuizBtn = document.getElementById('btn-start');
 const nextQuestionBtn = document.getElementById('next-questionBtn');
-const li = document.querySelectorAll('li');
-const ul = document.getElementById('list');
 const questionDiv = document.querySelector('.question');
+const ul = document.getElementById('list');
+const li = document.querySelectorAll('li');
 const answerOne = document.querySelector('.answer-one');
 const answerTwo = document.querySelector('.answer-two');
 const answerThree = document.querySelector('.answer-three');
 const answerFour = document.querySelector('.answer-four');
+const totalScore = document.getElementById('score');
+const playAgainBtn = document.getElementById('btn-play-again');
 
 let indexCount = 0;
 let score = 0;
@@ -19,98 +21,123 @@ const getQuiz = async () => {
 
   const data = await response.json();
 
-  let quizData = data.results;
+  const quizData = data.results;
+  console.log(quizData);
 
-  //console.log(data.results);
-  /* displayQuestion(quizData);
-  displayAnswers(quizData);
-  checkCorrectAnswer(quizData);
-  nextQuestion(quizData); */
-  playGame(quizData);
+  playQuiz(quizData);
 };
 
 startQuizBtn.addEventListener('click', () => {
-  checkInput();
-});
-checkInput = () => {
   if (questionsAmount.value === '') {
-    document.querySelector('.alert-message').style.display = 'inline-block';
+    document.querySelector('.alert-message').style.visibility = 'visible';
   } else {
-    UIinputs();
+    ul.style.display = 'block';
+    questionsAmount.style.display = 'none';
+    startQuizBtn.style.display = 'none';
+    document.getElementById('label-amount').style.display = 'none';
+    nextQuestionBtn.style.display = 'block';
+    document.querySelector('.answer-container').style.display = 'flex';
+    document.querySelector('.question').style.display = 'flex';
+    document.querySelector('.alert-message').style.display = 'none';
     getQuiz();
   }
+});
+
+playQuiz = quizData => {
+  displayQuestion(quizData);
+  shuffleAnswers();
+  displayAnswers(quizData);
+  checkCorrectAnswer(quizData);
+  nextQuestion(quizData);
+  resetGame();
 };
 
-UIinputs = () => {
-  questionsAmount.style.display = 'none';
-  startQuizBtn.style.display = 'none';
-  document.getElementById('label-amount').style.display = 'none';
-  nextQuestionBtn.style.display = 'block';
-  document.querySelector('.answer-container').style.display = 'flex';
-  document.querySelector('.question').style.display = 'flex';
-};
 displayQuestion = quizData => {
-  questionDiv.textContent = quizData[indexCount].question;
+  checkEndGameQuestions(quizData);
 };
 
 displayAnswers = quizData => {
-  answerOne.textContent = quizData[indexCount].correct_answer;
-  answerTwo.textContent = quizData[indexCount].incorrect_answers[0];
-  answerThree.textContent = quizData[indexCount].incorrect_answers[1];
-  answerFour.textContent = quizData[indexCount].incorrect_answers[2];
+  checkEndGameAnswers(quizData);
 };
+
+shuffleAnswers = () => {
+  const ul = document.querySelector('ul');
+  for (let i = ul.children.length; i >= 0; i--) {
+    ul.appendChild(ul.children[(Math.random() * i) | 0]);
+  }
+};
+
 nextQuestion = quizData => {
   nextQuestionBtn.addEventListener('click', () => {
-    nextIndex();
+    indexCount += 1;
     displayQuestion(quizData);
+    shuffleAnswers();
     displayAnswers(quizData);
     removeAnswerClasses();
   });
 };
+
 checkCorrectAnswer = quizData => {
-  ul.addEventListener('click', function(e) {
+  ul.addEventListener('click', e => {
     if (e.target.matches('li')) {
       e.target.parentElement.classList.add('disabled');
+      showCorrectAnswer(quizData);
       if (e.target.textContent === quizData[indexCount].correct_answer) {
         e.target.classList.add('correct-answer');
-        score++;
+        score += 1;
       } else {
         e.target.classList.add('incorrect-answer');
       }
     }
-
-    console.log(indexCount + 'index');
   });
 
-  /*  li.forEach(listItem => {
-    score++;
-    console.log(score);
-    listItem.addEventListener('click', () => {
-      listItem.parentElement.classList.add('disabled');
-      if (listItem.textContent === quizData[indexCount].correct_answer) {
-        listItem.classList.add('correct-answer');
-      } else {
-        listItem.classList.add('incorrect-answer');
-      }
+  removeAnswerClasses = () => {
+    li.forEach(function(listItem) {
+      listItem.classList.remove('incorrect-answer');
+      listItem.classList.remove('correct-answer');
+      listItem.parentElement.classList.remove('disabled');
     });
-  }); */
+  };
 };
 
-playGame = quizData => {
-  displayQuestion(quizData);
-  displayAnswers(quizData);
-  checkCorrectAnswer(quizData);
-  nextQuestion(quizData);
+checkEndGameQuestions = quizData => {
+  if (!(indexCount === parseInt(questionsAmount.value))) {
+    questionDiv.innerHTML = `<p>${quizData[indexCount].question}</p>`;
+  } else {
+    nextQuestionBtn.style.display = 'none';
+    questionDiv.style.display = 'none';
+    totalScore.style.display = 'flex';
+    showTotalScore();
+    ul.style.display = 'none';
+  }
 };
 
-nextIndex = () => {
-  indexCount += 1;
+checkEndGameAnswers = quizData => {
+  if (!(indexCount === parseInt(questionsAmount.value))) {
+    answerOne.textContent = quizData[indexCount].correct_answer;
+    answerTwo.textContent = quizData[indexCount].incorrect_answers[0];
+    answerThree.textContent = quizData[indexCount].incorrect_answers[1];
+    answerFour.textContent = quizData[indexCount].incorrect_answers[2];
+  }
 };
 
-removeAnswerClasses = () => {
-  li.forEach(function(listItem) {
-    listItem.classList.remove('incorrect-answer');
-    listItem.classList.remove('correct-answer');
-    listItem.parentElement.classList.remove('disabled');
+showTotalScore = () => {
+  let scoreCount = score;
+  totalScore.innerHTML = `<h1>Your total score is <span>${scoreCount}/${questionsAmount.value}</span></h1>
+  <h3>Play again?</h3>`;
+  playAgainBtn.style.display = 'block';
+};
+
+resetGame = () => {
+  playAgainBtn.addEventListener('click', function() {
+    location.reload();
+  });
+};
+
+showCorrectAnswer = quizData => {
+  li.forEach(function(answer) {
+    if (answer.textContent === quizData[indexCount].correct_answer) {
+      answer.classList.add('correct-answer');
+    }
   });
 };
