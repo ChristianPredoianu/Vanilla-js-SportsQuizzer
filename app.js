eadb0ab199fbaf16f039eb2b64e7ba748a5c53ad
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2020-07-07 10:32:33 +0200

    final version

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const questionsAmount = document.getElementById('amount-of-questions');
 const startQuizBtn = document.getElementById('btn-start');
 const nextQuestionBtn = document.getElementById('next-questionBtn');
 const questionDiv = document.querySelector('.question');
-const ul = document.getElementById('list');
-const li = document.querySelectorAll('li');
+const list = document.getElementById('list');
+const listItem = document.querySelectorAll('li');
 const answerOne = document.querySelector('.answer-one');
 const answerTwo = document.querySelector('.answer-two');
 const answerThree = document.querySelector('.answer-three');
@@ -11,133 +11,145 @@ const answerFour = document.querySelector('.answer-four');
 const totalScore = document.getElementById('score');
 const playAgainBtn = document.getElementById('btn-play-again');
 
-let indexCount = 0;
-let score = 0;
+let currentIndex = 0;
+let scoreCount = 0;
 
-const getQuiz = async () => {
+//Get quiz from API
+async function getQuiz(amountOfQuestions) {
   const response = await fetch(
-    `https://opentdb.com/api.php?amount=${questionsAmount.value}&category=21&type=multiple`
+    `https://opentdb.com/api.php?amount=${amountOfQuestions}&category=21&type=multiple`
   );
 
   const data = await response.json();
+  return data;
+}
 
-  const quizData = data.results;
-  console.log(quizData);
+//Initialize game
+function initGame() {
+  startQuizBtn.addEventListener('click', () => {
+    if (questionsAmount.value === '') {
+      alert('you must enter a valid number');
+    } else {
+      list.style.display = 'block';
+      questionsAmount.style.display = 'none';
+      startQuizBtn.style.display = 'none';
+      document.getElementById('label-amount').style.display = 'none';
+      nextQuestionBtn.style.display = 'block';
+      document.querySelector('.answer-container').style.display = 'flex';
+      document.querySelector('.question').style.display = 'flex';
+      document.querySelector('.alert-message').style.display = 'none';
 
-  playQuiz(quizData);
-};
-
-startQuizBtn.addEventListener('click', () => {
-  if (questionsAmount.value === '') {
-    document.querySelector('.alert-message').style.visibility = 'visible';
-  } else {
-    ul.style.display = 'block';
-    questionsAmount.style.display = 'none';
-    startQuizBtn.style.display = 'none';
-    document.getElementById('label-amount').style.display = 'none';
-    nextQuestionBtn.style.display = 'block';
-    document.querySelector('.answer-container').style.display = 'flex';
-    document.querySelector('.question').style.display = 'flex';
-    document.querySelector('.alert-message').style.display = 'none';
-    getQuiz();
-  }
-});
-
-playQuiz = (quizData) => {
-  displayQuestion(quizData);
-  shuffleAnswers();
-  displayAnswers(quizData);
-  checkCorrectAnswer(quizData);
-  nextQuestion(quizData);
-  resetGame();
-};
-
-displayQuestion = (quizData) => {
-  checkEndGameQuestions(quizData);
-};
-
-displayAnswers = (quizData) => {
-  checkEndGameAnswers(quizData);
-};
-
-shuffleAnswers = () => {
-  const ul = document.querySelector('ul');
-  for (let i = ul.children.length; i >= 0; i--) {
-    ul.appendChild(ul.children[(Math.random() * i) | 0]);
-  }
-};
-
-nextQuestion = (quizData) => {
-  nextQuestionBtn.addEventListener('click', () => {
-    indexCount += 1;
-    displayQuestion(quizData);
-    shuffleAnswers();
-    displayAnswers(quizData);
-    removeAnswerClasses();
-  });
-};
-
-checkCorrectAnswer = (quizData) => {
-  ul.addEventListener('click', (e) => {
-    if (e.target.matches('li')) {
-      e.target.parentElement.classList.add('disabled');
-      showCorrectAnswer(quizData);
-      if (e.target.textContent === quizData[indexCount].correct_answer) {
-        e.target.classList.add('correct-answer');
-        score += 1;
-      } else {
-        e.target.classList.add('incorrect-answer');
-      }
+      getQuiz(questionsAmount.value).then((data) => {
+        playGame(data);
+        nextQuestionBtn.classList.add('disabled');
+        console.log(data);
+      });
     }
   });
+}
 
-  showCorrectAnswer = (quizData) => {
-    li.forEach((answer) => {
-      if (answer.textContent === quizData[indexCount].correct_answer) {
-        answer.classList.add('correct-answer');
-      }
-    });
-  };
+//Play game
+function playGame(data) {
+  displayQuestion(data);
+  displayAnswers(data);
+  shuffleAnswers();
+  checkCorrectAnswer(data);
+  removeAnswerClasses();
+  nextQuestion(data);
+  resetGame();
+}
 
-  removeAnswerClasses = () => {
-    li.forEach(function (listItem) {
-      listItem.classList.remove('incorrect-answer');
-      listItem.classList.remove('correct-answer');
-      listItem.parentElement.classList.remove('disabled');
-    });
-  };
-};
+//Display questions
+function displayQuestion(data) {
+  questionDiv.textContent = data.results[currentIndex].question;
+}
 
-checkEndGameQuestions = (quizData) => {
-  if (!(indexCount === parseInt(questionsAmount.value))) {
-    questionDiv.innerHTML = `<p>${quizData[indexCount].question}</p>`;
-  } else {
-    nextQuestionBtn.style.display = 'none';
-    questionDiv.style.display = 'none';
-    totalScore.style.display = 'flex';
-    showTotalScore();
-    ul.style.display = 'none';
+//Display answers
+function displayAnswers(data) {
+  answerOne.textContent = data.results[currentIndex].correct_answer;
+  answerTwo.textContent = data.results[currentIndex].incorrect_answers[0];
+  answerThree.textContent = data.results[currentIndex].incorrect_answers[1];
+  answerFour.textContent = data.results[currentIndex].incorrect_answers[2];
+}
+
+//Shuffle answers
+function shuffleAnswers() {
+  for (let i = list.children.length; i >= 0; i--) {
+    list.appendChild(list.children[(Math.random() * i) | 0]);
   }
-};
+}
 
-checkEndGameAnswers = (quizData) => {
-  if (!(indexCount === parseInt(questionsAmount.value))) {
-    answerOne.textContent = quizData[indexCount].correct_answer;
-    answerTwo.textContent = quizData[indexCount].incorrect_answers[0];
-    answerThree.textContent = quizData[indexCount].incorrect_answers[1];
-    answerFour.textContent = quizData[indexCount].incorrect_answers[2];
-  }
-};
+//Check right answer
+function checkCorrectAnswer(data) {
+  list.addEventListener('click', (e) => {
+    if (e.target.matches('li')) {
+      e.target.parentElement.classList.add('disabled');
+      nextQuestionBtn.classList.remove('disabled');
+    }
+    if (e.target.textContent === data.results[currentIndex].correct_answer) {
+      e.target.classList.add('correct-answer');
+      console.log(e.target);
+      scoreCount += 1;
+    } else {
+      e.target.classList.add('incorrect-answer');
+      showCorrectAnswer(data);
+      console.log(e.target);
+    }
+  });
+}
 
-showTotalScore = () => {
-  let scoreCount = score;
+//Show user the correct answer if wrong answer
+function showCorrectAnswer(data) {
+  listItem.forEach((answer) => {
+    if (answer.textContent === data.results[currentIndex].correct_answer) {
+      answer.classList.add('correct-answer');
+    }
+  });
+}
+
+//Remove correct/wrong/disabled classes
+function removeAnswerClasses() {
+  listItem.forEach(function (item) {
+    item.classList.remove('incorrect-answer');
+    item.classList.remove('correct-answer');
+    item.parentElement.classList.remove('disabled');
+  });
+}
+
+//Next question
+function nextQuestion(data) {
+  nextQuestionBtn.addEventListener('click', () => {
+    nextQuestionBtn.classList.add('disabled');
+    if (currentIndex !== parseInt(questionsAmount.value - 1)) {
+      currentIndex += 1;
+      displayQuestion(data);
+      displayAnswers(data);
+      shuffleAnswers();
+      removeAnswerClasses();
+      console.log('index' + currentIndex);
+      console.log('amount of questions' + questionsAmount.value);
+    } else {
+      nextQuestionBtn.style.display = 'none';
+      questionDiv.style.display = 'none';
+      totalScore.style.display = 'flex';
+      list.style.display = 'none';
+      showTotalScore();
+    }
+  });
+}
+
+//Showing total score
+function showTotalScore() {
   totalScore.innerHTML = `<h1>Your total score is <span>${scoreCount}/${questionsAmount.value}</span></h1>
   <h3>Play again?</h3>`;
   playAgainBtn.style.display = 'block';
-};
+}
 
-resetGame = () => {
+//Reset game/reload page
+function resetGame() {
   playAgainBtn.addEventListener('click', function () {
     location.reload();
   });
-};
+}
+
+initGame();
